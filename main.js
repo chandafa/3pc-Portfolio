35b29835820109e8fc51d66a0ad13af6ec44d598
chandafa <chankirana722@gmail.com>
2023-07-26 22:00:52 +0700

    first commit

--- main.js
+++ main.js
@@ -317,7 +317,7 @@ function loadIntroText() {
       new THREE.MeshPhongMaterial({ color: 0x171f27, flatShading: true }),
       new THREE.MeshPhongMaterial({ color: 0xffffff }),
     ];
-    const titleGeo = new TextGeometry('SUSHIL THAPA', {
+    const titleGeo = new TextGeometry('CANDRA KIRANA', {
       font: font,
       size: 0.08,
       height: 0.01,
@@ -334,7 +334,7 @@ function loadIntroText() {
       new THREE.MeshPhongMaterial({ color: 0xffffff }),
     ];
     const subTitleGeo = new TextGeometry(
-      'Web Designer / Developer / Content Creator',
+      'Designer / Developer / Univercity Student',
       {
         font: font,
         size: 0.018,
